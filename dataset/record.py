d11c98948873b8ac2ff38ac6406ad40e927ebcfa
zafar <zafar142007@gmail.com>
2020-04-23 18:47:41 +0530

    added record script

---
+++ dataset/record.py
@@ -0,0 +1,31 @@
+import pyaudio
+import sys
+
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 5
+TOTAL_RECORD_TIME_MIN=1 #in minutes
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS, 
+                rate=RATE, 
+                input=True,
+                output=True,
+                frames_per_buffer=chunk)
+
+print "* recording"
+for i in range(TOTAL_RECORD_TIME_MIN*60/RECORD_SECONDS):
+	for i in range(0, 44100 / chunk * RECORD_SECONDS):
+	    data = stream.read(chunk)
+	    # check for silence here by comparing the level with 0 (or some threshold) for 
+	    # the contents of data.
+	    # then write data or not to a file
+
+print "* done"
+
+stream.stop_stream()
+stream.close()
+p.terminate()
